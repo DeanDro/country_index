bea639b98d157c7bfc06cf1f3c7e027a1b766f57
Konstantinos Drosos <dean.drossos@gmail.com>
2022-08-24 00:21:55 -0400

    Adding ReactJS frontend

---
+++ frontend/frontend/src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Welcome(props){
+    return (
+        <div className='introduction'>
+            <h3>Country Indexing Application</h3>
+        </div>
+    );
+}
+
+export default Welcome;
