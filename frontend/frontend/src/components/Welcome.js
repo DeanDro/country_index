b38adfcd7e5a108963ac93955fbfba601942185f
Konstantinos Drosos <dean.drossos@gmail.com>
2022-08-26 16:31:42 -0400

    Setting up front end and database interaction

--- frontend/frontend/src/components/Welcome.js
+++ frontend/frontend/src/components/Welcome.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Welcome(props){
     return (
         <div className='introduction'>
-            <h3>Country Indexing Application</h3>
+            <div className='container'>
+                <h2>Country Indexing Application</h2>
+                <br></br>
+                <p><b>Instructions:</b>Select the top 10 categories you consider most important to view 
+                a ranking of each country based on how they perform in these areas. The information are collected 
+                by publicly available sources and analysed based on your priorities list.</p>
+            </div>
         </div>
     );
 }
